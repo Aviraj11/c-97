cde23185c36ea5cdfda8d8f867e92ad82d79be86
Aviraj11 <66950025+Aviraj11@users.noreply.github.com>
2021-06-01 21:05:53 +0530

    Update guessingGame.py

--- Guessing game/guessingGame.py
+++ Guessing game/guessingGame.py
@@ -5,11 +5,11 @@ chances = 5
 while chances > 0:
     guess = int(input("Guess a number between 1-9. You have 5 chances to guess the number!"))
     
-    if guess<number:
+    if guess>number:
         print("You guessed too High!")
         chances = chances -1
         continue
-    elif guess>number:
+    elif guess<number:
         print("You Guessed too Low!")
         chances = chances -1
         continue
